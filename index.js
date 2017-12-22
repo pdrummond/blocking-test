ac940d409331bf1dd374702000b8c2b228478653
Paul Drummond <paul.drummond@iode.co.uk>
2017-12-22 12:55:33 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/one', (req, res) => res.send('Boom! /one'))
+app.get('/two', (req, res) => {
+    for(i = 0; i<999999999; i++) {
+        console.log("Loop " + i);
+    }
+    res.send('Boom! /two')
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
