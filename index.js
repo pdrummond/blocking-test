56456eddd99b020e0563c0d1366bae139f77b7be
Paul Drummond <paul.drummond@iode.co.uk>
2018-01-11 15:50:15 +0000

    Added better launch log

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/one', (req, res) => res.send('Boom! /one'))
+app.get('/one', (req, res) => res.send('Boom! /one'));
 app.get('/two', (req, res) => {
-    for(i = 0; i<999999999; i++) {
-        console.log("Loop " + i);
-    }
-    res.send('Boom! /two')
+  for (i = 0; i < 999999999; i++) {
+    console.log('Loop ' + i);
+  }
+  res.send('Boom! /two');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('BLOCKING TEST app listening on port 3000!'));
